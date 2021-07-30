f635e6f90d730051aa563282ab1d4c5137d6ea2b
kauve <carlionidas2002@gmail.com>
2021-07-29 21:16:17 -0500

    29/07

---
+++ js/cambiar-color.js
@@ -0,0 +1,8 @@
+function colorChanger() {
+    var selectValue = document.getElementById("mySelection").value;
+    document.getElementById("dummy").innerHTML = " " + selectValue;
+    document.body.style.backgroundColor = selectValue;
+
+    var tono = document.getElementsByClassName("colorear:before")
+
+}
